4f92bd1a1cd4f1a10eba013b9f5a97f406dcdc38
1gn1z_el_angel <kebor327@gmail.com>
2022-01-09 22:13:07 -0600

    2. demas importaciones

--- book_register.py
+++ book_register.py
@@ -4,6 +4,16 @@
 # Importamos todo peewee
 from peewee import *
 
+# Para usar el diccionario ordenado para implementar menú con letras y no con números.
+from collections import OrderedDict
+
+import sys
+
+# Para usar horas y fechas en python
+import datetime
+
+# Para pedir el password de forma segura (no se ven las letras al escribir el password)
+import getpass
 
 
 
